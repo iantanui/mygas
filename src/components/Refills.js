ce515069c18d4b58630b56ef6b152e116eab2a1c
iantanui <rutooian@gmail.com>
2024-06-05 16:51:11 +0300

    Refills

---
+++ src/components/Refills.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Refills = () => {
+    return (
+        <div>
+            <h1>Refills</h1>
+
+            { /* Refills */ }
+        </div>
+    )
+}
+
+export default Refills;
