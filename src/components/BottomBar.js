c8c6edc0501237767356c58ef12b7ab3db005467
iantanui <rutooian@gmail.com>
2024-06-05 17:23:54 +0300

    stick to bottom

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import { BottomNavigation, BottomNavigationAction } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
-import ListIcon from '@mui/icons-material/List';
-import LocalGasStationIcon from '@mui/icons-material/LocalGasStation';
-import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import React from "react";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import ListIcon from "@mui/icons-material/List";
+import LocalGasStationIcon from "@mui/icons-material/LocalGasStation";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 
 const BotttomBar = ({ value, onchange }) => {
-    return (
-        <BottomNavigation value={value} onChange={onchange}>
-            <BottomNavigationAction label="Dashboard" value="dashboard" icon={<HomeIcon />} />
-            <BottomNavigationAction label="Products" value="products" icon={<ListIcon />} />
-            <BottomNavigationAction label="Refills" value="refills" icon={<LocalGasStationIcon />} />
-            <BottomNavigationAction label="Management" value="management" icon={<ManageAccountsIcon />} />
-        </BottomNavigation>
-    );
-}; 
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={onchange}
+      style={{
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+        backgroundColor: "white",
+        boxShadow: "0px -2px 4px rgba(0, 0, 0, 0.1)",
+        zIndex: 100,
+      }}
+    >
+      <BottomNavigationAction
+        label="Dashboard"
+        value="dashboard"
+        icon={<HomeIcon />}
+      />
+      <BottomNavigationAction
+        label="Products"
+        value="products"
+        icon={<ListIcon />}
+      />
+      <BottomNavigationAction
+        label="Refills"
+        value="refills"
+        icon={<LocalGasStationIcon />}
+      />
+      <BottomNavigationAction
+        label="Management"
+        value="management"
+        icon={<ManageAccountsIcon />}
+      />
+    </BottomNavigation>
+  );
+};
 
 export default BotttomBar;
