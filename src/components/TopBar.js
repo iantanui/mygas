83aca025327aecd082d3f4315c9d81d40b3f39f2
iantanui <rutooian@gmail.com>
2024-06-05 16:38:19 +0300

    TopBar

---
+++ src/components/TopBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+
+const TopBar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h5" style={{ flexGrow: 1 }}>
+          myGas
+        </Typography>
+        <IconButton edge="end" color="inherit">
+            <AccountCircle />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Toolbar;
