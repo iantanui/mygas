48e6a5f1c5cfbbc91009597da218abae9c88dda2
iantanui <rutooian@gmail.com>
2024-06-05 18:07:42 +0300

    app bar styling

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,13 +4,18 @@ import AccountCircle from "@mui/icons-material/AccountCircle";
 
 const TopBar = () => {
   return (
-    <AppBar position="static" style={{ backgroundColor: "white", color: "black"}}>
-      <Toolbar>
-        <Typography variant="h5" style={{ flexGrow: 1 }}>
-          myGas
-        </Typography>
+    <AppBar
+      position="static"
+      style={{ backgroundColor: "white", color: "black", boxShadow: "none" }}
+    >
+      <Toolbar style={{ justifyContent: "space-between" }}>
+        <div className="title">
+          <Typography variant="h4">myGas</Typography>
+          <Typography variant="p">Crystal management made easy</Typography>
+        </div>
+
         <IconButton edge="end" color="inherit">
-            <AccountCircle />
+          <AccountCircle />
         </IconButton>
       </Toolbar>
     </AppBar>
