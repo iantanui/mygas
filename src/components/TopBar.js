9cc8009fc1c8f4c8286f46a0ce656558c121659c
iantanui <rutooian@gmail.com>
2024-06-06 20:39:34 +0300

    text

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import '../App.css'
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "../App.css";
+import { Person } from "react-bootstrap-icons";
 
 const TopBar = () => {
   return (
     <Navbar bg="dark" variant="dark" className="fixed-top">
-      <Navbar.Brand href="/">myGas</Navbar.Brand>
+      <div className="top">
+        <Navbar.Brand className="title">myGas</Navbar.Brand>
+        <Navbar.Brand className="sub-title">
+          Crystal management made easy
+        </Navbar.Brand>
+      </div>
       <Nav>
-        <Nav.Link href="/account">Account</Nav.Link>
+        <Nav.Link href="/account">
+          <Person />
+        </Nav.Link>
       </Nav>
     </Navbar>
   );
