d8687325219829313c11b1d3e60900e712b85e41
iantanui <rutooian@gmail.com>
2024-06-11 12:54:20 +0300

    dialog

--- src/components/products/ProductDialog.js
+++ src/components/products/ProductDialog.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ProductDialog({ product, onDismiss, onSave }) {
-  
-  return <div></div>;
+  const [name, setName] = useState(product ? product.name : "");
+  const [quantity, setQuantity] = useState(product ? product.quantity : 0);
+  const [price, setPrice] = useState(product ? product.price : 0.0);
+
+  const handleSubmit = () => {
+    onSave({ id: product?.id, name, quantity, price });
+  };
+
+  return (
+    <div className="dialog">
+      <h2>{product ? "Edit Product" : "Add Product"}</h2>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        Quantity:{" "}
+        <input
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        />
+      </label>
+      <label>
+        Price:
+        <input
+          type="number"
+          value={price}
+          onChange={(e) => setPrice(Number(e.target.value))}
+        />
+      </label>
+      <button onClick={handleSubmit}>Save</button>
+      <button onClick={onDismiss}>Cancel</button>
+    </div>
+  );
 }
 
 export default ProductDialog;
