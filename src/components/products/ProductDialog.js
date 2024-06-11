881b56e021dccd9e1d39782c9820c67c7b731be4
iantanui <rutooian@gmail.com>
2024-06-11 11:59:42 +0300

    product dialog

--- src/components/products/ProductDialog.js
+++ src/components/products/ProductDialog.js
@@ -1,53 +1,8 @@
-import React from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
+import React from "react";
 
-const ProductDialog = ({ show, handleClose, handleSave, currentProduct, handleChange, modalType }) => {
-  return (
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>{modalType} Product</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Form.Group controlId="formProductName">
-            <Form.Label>Product Name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter product name"
-              name="name"
-              value={currentProduct.name}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group controlId="formProductQuantity" className="mt-3">
-            <Form.Label>Quantity</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Enter quantity"
-              name="quantity"
-              value={currentProduct.quantity}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group controlId="formProductPrice" className="mt-3">
-            <Form.Label>Price</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Enter price"
-              name="price"
-              value={currentProduct.price}
-              onChange={handleChange}
-            />
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>Close</Button>
-        <Button variant="primary" onClick={handleSave}>Save</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-};
+function ProductDialog({ product, onDismiss, onSave }) {
+  
+  return <div></div>;
+}
 
 export default ProductDialog;
-
--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -1,58 +1,7 @@
-import React, { useState } from "react";
-import { Card, Button, Modal, ModalTitle } from "react-bootstrap";
-import "./ProductItem.css";
-import { ThreeDots } from "react-bootstrap-icons";
+import React from "react";
 
-const ProductItem = ({ product, handleShowModal }) => {
-  const [showModal, setShowModal] = useState(false);
-
-  const handleToggleMore = () => {
-    setShowModal(!showModal);
-  };
-  return (
-    <Card className="product-card">
-      <div className="product-card-header">
-        <div className="product-id">{product.id}</div>
-        <div className="product-actions">
-          <ThreeDots
-            size={20}
-            onClick={handleToggleMore}
-            style={{ cursor: "pointer" }}
-          />
-        </div>
-      </div>
-
-      <div className="divider" />
-
-      <div className="product-info">
-        <h5>{product.name}</h5>
-        <p>{product.quantity}</p>
-        <p>${product.price}</p>
-      </div>
-
-      <Modal show={showModal} onHide={handleToggleMore} centered>
-        <Modal.Header closeButton>
-          <ModalTitle>Product</ModalTitle>
-        </Modal.Header>
-        <Modal.Body>
-          <Button
-            variant="outline-primary"
-            size="sm"
-            className="me-1"
-            onClick={() => handleShowModal("Edit", product)}
-          >
-            Edit
-          </Button>
-          <Button variant="outline-success" size="sm" className="me-1">
-            View
-          </Button>
-          <Button variant="outline-danger" size="sm">
-            Delete
-          </Button>
-        </Modal.Body>
-      </Modal>
-    </Card>
-  );
-};
+function ProductItem({}) {
+  return <div></div>;
+}
 
 export default ProductItem;
