b4d3f5e09111c3e83162a80132e18916f48c9d9a
iantanui <rutooian@gmail.com>
2024-06-11 11:52:24 +0300

    botoombar

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,44 +1,15 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { DatabaseCheck, Droplet, Grid, Person, Sliders2 } from "react-bootstrap-icons";
+import { NavLink } from "react-router-dom";
 import "../App.css";
 
-const BottomBar = () => {
+function BottomBar () {
   return (
-    <Nav
-      fill
-      variant="tabs"
-      defaultActiveKey="/dashboard"
-      className="fixed-bottom"
-      style={{ }}
-    >
-      <Nav.Item className="nav">
-        <Nav.Link as={Link} to="/dashboard" className="link">
-          <Grid /> Dashboard
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={Link} to="/products"  className="link">
-          <Droplet /> Products
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={Link} to="/refills"  className="link">
-          <DatabaseCheck className="icon"/> Refills
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={Link} to="/management"  className="link">
-          <Sliders2 />Management
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={Link} to="/account"  className="link">
-          <Person />Account
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <div className="bottom-nav-bar">
+        <NavLink to="/dashboard" activeClassName="active"> Dashboard</NavLink>
+        <NavLink to="/products" activeClassName="active">Products</NavLink>
+        <NavLink to="/refills" activeClassName="active">Refills</NavLink>
+        <NavLink to="/manage" activeClassName="active"> Manage</NavLink>
+    </div>
   );
 };
 
