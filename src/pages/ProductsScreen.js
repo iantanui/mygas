dff6c789308c6ab36ad3a281c52f7d9367558a2c
iantanui <rutooian@gmail.com>
2024-06-11 11:39:53 +0300

    product dialog

--- src/pages/ProductsScreen.js
+++ src/pages/ProductsScreen.js
@@ -1,96 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductItem from "../components/products/ProductItem";
 import ProductDialog from "../components/products/ProductDialog";
 
-function ProductsScreen () {
+function ProductsScreen() {
   const [products, setProducts] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [modalType, setModalType] = useState("");
-  const [currentProduct, setCurrentProduct] = useState({
-    id: "",
-    name: "",
-    quantity: "",
-    price: "",
-  });
+  const [showDialog, setShowDialog] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState(null);
 
-  const handleShowModal = (type, product) => {
-    setModalType(type);
-    if (products) {
-      setCurrentProduct(product);
+  useEffect(() => {
+    fetchProducts().then(setProducts);
+  }, []);
+
+  const handleSave = (product) => {
+    if (currentProduct) {
+      updatedProduct(product).then(() => {
+        setProducts(products.map((p) => (p.id === product.id ? product : p)));
+      });
     } else {
-      setCurrentProduct({ id: "", name: "", quantity: "", price: "" });
+      addProduct(product).then((newProduct) => {
+        setProducts([...products, newProduct]);
+      });
     }
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => setShowModal(false);
-
-  const handleSave = () => {
-    if (modalType === "Add") {
-      const newProduct = { ...currentProduct, id: products.length + 1 };
-      setProducts([...products, newProduct]);
-    } else if (modalType === "Edit") {
-      const updatedProducts = products.map((product) =>
-        product.id === currentProduct.id ? currentProduct : product
-      );
-      setProducts(updatedProducts);
-    }
-    handleCloseModal();
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setCurrentProduct({ ...currentProduct, [name]: value });
+    setShowDialog(false);
+    setCurrentProduct(null);
   };
 
   return (
-    <Container className="mt-5">
-      <Row className="mb-4">
-        <Col>
-          <h1>Products</h1>
-          <p>List of products</p>
-        </Col>
-
-        <Col className="text-end">
-          <Calendar size={24} />
-        </Col>
-      </Row>
-      
-      <Row>
-        <Col>
-          <Button variant="primary" onClick={() => handleShowModal("Add")}>
-            Add Product
-          </Button>
-        </Col>
-      </Row>
-      <Row className="my-3">
-        <Col>
-          {products.map((product) => (
-            <ProductItem
-              key={product.id}
-              product={product}
-              handleShowModal={handleShowModal}
-            />
-          ))}
-        </Col>
-      </Row>
-      <ProductDialog
-        show={showModal}
-        handleClose={handleCloseModal}
-        handleSave={handleSave}
-        currentProduct={currentProduct}
-        handleChange={handleChange}
-        modalType={modalType}
-      />
-    </Container>
-
-    // <div className="container">
-    //   <h1>Products</h1>
-    //   {/* Add products content here */}
-    //   { /* </ProductItem /> */}
-    //   {/* <ProductDialog /> */}
-    // </div>
+    <div>
+      <button onClick={() => setShowDialog(true)}>Add Product</button>
+        {products.map(product => (
+          key={product.id}
+          product={product}
+          onEdit={() => {
+          setCurrentProduct(product);
+          setShowDialog(true);
+          }}
+          onDelete={() => {
+            deleteProduct(product.id).then(() => {
+              setProducts(products.filter(p => p.id !== product.id));
+            });
+          }}
+          />
+        ))}
+        {showDialog && (
+          <ProductDialog
+          product={currentProduct}
+          onDismiss={() => setShowDialog(false)}
+          onSave={handleSave}
+          />
+        )}
+        </div>
   );
-};
+}
 
 export default ProductsScreen;
