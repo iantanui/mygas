a71198a0679975be5d5cb12259d9679b766f5dff
iantanui <rutooian@gmail.com>
2024-06-11 11:43:54 +0300

    apis

---
+++ src/api.js
@@ -0,0 +1,31 @@
+export const fetchProducts = async () => {
+
+};
+
+export const addProduct = async (product) => {
+
+};
+
+export const updateProduct = async (product) => {
+
+};
+
+export const deleteProduct = async (productId) => {
+
+};
+
+export const fetchSales = async () => {
+
+};
+
+export const addSale = async (sale) => {
+
+};
+
+export const updateSale = async (sale) => {
+
+};
+
+export const deleteSale = async (saleId) => {
+
+};
