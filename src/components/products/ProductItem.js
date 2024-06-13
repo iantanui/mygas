2bd8371f06743a04ed92c5bae35737e6a481644d
iantanui <rutooian@gmail.com>
2024-06-13 13:23:57 +0300

    product item

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -1,14 +1,64 @@
-import React from "react";
+import {
+  Card,
+  CardContent,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { MoreVert } from "@mui/icons-material";
 
-function ProductItem({ product, onEdit, onDelete }) {
+function ProductItem({ index, product, onEdit, onDelete }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div className="product-item">
-      <span>{product.name}</span>
-      <span>{product.quantity}</span>
-      <span>{product.price}</span>
-      <button onClick={onEdit}>Edit</button>
-      <button onClick={onDelete}>Delete</button>
-    </div>
+    <Card>
+      <CardContent>
+        <Typography>{index + 1}</Typography>
+        <IconButton onClick={handleMenuOpen}>
+          <MoreVert />
+        </IconButton>
+
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+        >
+          <MenuItem
+            onClick={() => {
+              onEdit(product);
+              handleMenuClose();
+            }}
+          >
+            Edit
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              onDelete(product.id);
+              handleMenuClose();
+            }}
+          >
+            Delete
+          </MenuItem>
+        </Menu>
+
+        <Divider />
+
+        <Typography>Name: {product.name}</Typography>
+        <Typography>Quantity: {product.quantity}</Typography>
+        <Typography>Price: {product.price}</Typography>
+      </CardContent>
+    </Card>
   );
 }
 
