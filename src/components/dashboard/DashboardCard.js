0ed7748e5e406a48370f850b1af5f87e15db3f8d
iantanui <rutooian@gmail.com>
2024-06-17 11:13:34 +0300

    styles

--- src/components/dashboard/DashboardCard.js
+++ src/components/dashboard/DashboardCard.js
@@ -3,20 +3,32 @@ import React from "react";
 
 const DashboardCard = ({ icon, label, value, bottom }) => {
   return (
-    <Card style={{ height: "120px", backgroundColor: "white" }} elevation={8}>
-      <CardContent>
+    <Card
+      style={{
+        height: "120px",
+        backgroundColor: "white",
+        border: "1px solid lightgray",
+        borderRadius: '10px'
+      }}
+    >
+      <CardContent style={{ padding: "8px" }}>
         <Box display="flex" justifyContent="space-between" alignItems="center">
           <Typography variant="body2" color="black">
             {label}
           </Typography>
           {icon}
         </Box>
-        <Box display="flex" justifyContent="center" alignItems="center" my={2}>
+        <Box
+          display="flex"
+          justifyContent="flex-start"
+          alignItems="center"
+          my={2}
+        >
           <Typography variant="h6" color="black" fontWeight="bold">
             {value}
           </Typography>
         </Box>
-        <Typography variant="caption" color="lightgray" align="center">
+        <Typography variant="caption" color="gray" align="center">
           {bottom}
         </Typography>
       </CardContent>
