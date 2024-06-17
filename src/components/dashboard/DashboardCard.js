8c9da1187d26824c300875a11f77488a3300d609
iantanui <rutooian@gmail.com>
2024-06-17 15:29:28 +0300

    done

--- src/components/dashboard/DashboardCard.js
+++ src/components/dashboard/DashboardCard.js
@@ -5,13 +5,15 @@ const DashboardCard = ({ icon, label, value, bottom }) => {
   return (
     <Card
       style={{
-        height: "120px",
+        height: "80%",
+        padding: "8px",
         backgroundColor: "white",
         border: "1px solid lightgray",
-        borderRadius: '10px'
+        borderRadius: "10px",
+        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
       }}
     >
-      <CardContent style={{ padding: "8px" }}>
+      <CardContent style={{ padding: "4px" }}>
         <Box display="flex" justifyContent="space-between" alignItems="center">
           <Typography variant="body2" color="black">
             {label}
