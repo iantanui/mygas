c3d384ab40766da57d13f4331ab77c49a38eca75
iantanui <rutooian@gmail.com>
2024-06-17 10:08:52 +0300

    dashboardcard

--- src/components/dashboard/DashboardCard.js
+++ src/components/dashboard/DashboardCard.js
@@ -1,1 +1,21 @@
-import React from 'react';
+import { Box, Card, CardContent, Typography } from "@mui/material";
+import React from "react";
+
+const DashboardCard = ({ icon, label, value, bottom }) => {
+  return (
+    <Card>
+      <CardContent>
+        <Box>
+          <Typography>{label}</Typography>
+          {icon}
+        </Box>
+        <Box>
+          <Typography>{value}</Typography>
+        </Box>
+        <Typography>{bottom}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default DashboardCard;
